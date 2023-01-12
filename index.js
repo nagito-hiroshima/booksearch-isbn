39aed1396d183bfb7c85360919905b4042e5d1e2
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2023-01-12 10:51:33 +0900

    表追加

--- index.js
+++ index.js
@@ -2,22 +2,33 @@
 function test() {
     var table = document.getElementById('foo-table');  //表のオブジェクトを取得
 
+    //DB分繰り返し表作成
     for (j = 1; j < jsonObj.length; j++) {
+
+        /* 初期設定エリアここまで　↓↓↓↓↓↓↓ */
         var row = table.insertRow(-1);  //行末に行(tr要素)を追加
         var cell = row.insertCell(0);  //セル(td要素)の追加
-        for (q = 1; q < 25; q++) {
+        for (q = 1; q < 26; q++) {//表の数分設定
+            // cell(q) = row.insertCell(q);の変数名宣言
             eval('var cell' + q + ' = row.insertCell(' + q + ');')
         }
+        /* 初期設定エリアここまで　↑↑↑↑↑↑ */
 
+
+        /* 行作成エリア　↓↓↓↓↓↓↓ */
         cell.innerHTML = "<img class='book_img' src='" + jsonObj[j][17] + "'>"
         cell1.innerHTML = "<div class='book_view'><div class='book_title'>" + jsonObj[j][1] + "</div><div class='book_name'>" + jsonObj[j][7] + "</div><div class='book_cp'> " + jsonObj[j][9] + "</div><div class='book_data'>" + jsonObj[1][10].substr(0, 10) + "</div></div>"
-        for (i = 2; i < 25; i++) {
+
+        for (i = 2; i < 25; i++) {//自動セル書き込み
             eval('cell' + i + '.innerHTML= "<div class=table"+' + i + ' +">" + jsonObj[' + j + '][' + (i - 2) + ']+" </div>"')
         }
+
+        cell25.innerHTML = ""
+        /* 行作成エリアここまで　↑↑↑↑↑↑ */
     }
 
-
     table.deleteRow(1);
+
 
     jQuery(function ($) {
         // デフォルトの設定を変更
